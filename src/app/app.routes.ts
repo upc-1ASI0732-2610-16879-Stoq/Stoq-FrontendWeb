cd5f1ab14e5279bc3fb3cdd007357d54d4555527
Dayro <dayro.rios.p@gmail.com>
2025-10-07 21:57:13 -0500

    feat(routes): updated route for providers view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './shared/presentation/views/home/home';
 import { InventoryListComponent } from './inventory/presentation/inventory-list/inventory-list';
+import {ProvidersTable} from './providers-management/presentation/components/providers-table/providers-table';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'inventario', component: InventoryListComponent },
-  { path: 'proveedores', component: HomeComponent },
+  { path: 'proveedores', component: ProvidersTable },
   { path: 'venta', component: HomeComponent },
   { path: 'reportes', component: HomeComponent },
   { path: 'configuracion', component: HomeComponent },
