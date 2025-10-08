f510686dde4c07c920291950be694632533864c1
Dayro <dayro.rios.p@gmail.com>
2025-10-08 03:54:13 -0500

    chore: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './shared/presentation/views/home/home';
 import { InventoryListComponent } from './inventory/presentation/inventory-list/inventory-list';
 import {ProvidersTable} from './providers-management/presentation/components/providers-table/providers-table';
+import {ProviderComponent} from './shared/presentation/views/provider/provider';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'inventario', component: InventoryListComponent },
-  { path: 'proveedores', component: ProvidersTable },
+  { path: 'proveedores', component: ProviderComponent },
   { path: 'venta', component: HomeComponent },
   { path: 'reportes', component: HomeComponent },
   { path: 'configuracion', component: HomeComponent },
