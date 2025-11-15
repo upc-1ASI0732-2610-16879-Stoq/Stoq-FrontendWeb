c9259086228eed1ce5392bea00b77cdc71d642ed
Piero Sulca <psulcasanchez@gmail.com>
2025-11-15 10:20:03 -0500

    chore: update API base URLs in development and production environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://stocktrack-fake-api-production.up.railway.app',
-  platformProviderApiBaseUrl: 'https://stocktrack-fake-api-production.up.railway.app',
+  apiBaseUrl: 'https://stocktrack-fake-api.vercel.app',
+  platformProviderApiBaseUrl: 'https://stocktrack-fake-api.vercel.app',
   platformProviderProvidersEndpointPath: '/providers',
   platformProviderProductsEndpointPath: '/products',
   platformProviderRestockingEndpointPath: '/restockings',
